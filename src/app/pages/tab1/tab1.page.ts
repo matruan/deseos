79871c87331b89a90a6b9dad9b6129374452b68c
Matias Ruiz <ruizmat@gmail.com>
2020-05-31 19:03:44 +0200

    second commit - adding models

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { DeseosService } from './../../services/deseos.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public deseosService: DeseosService) {}
 
 }
