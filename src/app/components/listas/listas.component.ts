92451b2de87e6eedddbc76b90a1442717d36387b
Matias Ruiz <ruizmat@gmail.com>
2020-10-19 17:02:14 +0200

    Adding edit list feature

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,8 +1,9 @@
+import { TabsPageRoutingModule } from './../../pages/tabs/tabs-routing.module';
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Lista } from 'src/app/models/lista.model';
 import { Router } from '@angular/router';
 import { DeseosService } from './../../services/deseos.service';
-import { IonList } from '@ionic/angular';
+import { IonList, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -13,7 +14,7 @@ export class ListasComponent implements OnInit {
 
   @ViewChild(IonList, {static: false}) lista: IonList;
   @Input() terminada = true;
-  constructor(public deseosService: DeseosService, private router: Router) {
+  constructor(public deseosService: DeseosService, private router: Router, private alertController: AlertController) {
   }
   ngOnInit() {}
 
@@ -27,6 +28,44 @@ export class ListasComponent implements OnInit {
 
   borrarLista(lista: Lista) {
     this.deseosService.borrarLista(lista);
+  }
+
+  async editarNombre(lista: Lista) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Editar nombre',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          value: lista.titulo,
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.lista.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Guardar',
+          handler: (data) => {
+            if (data.titulo.length === 0) {
+              return;
+            }
+            lista.titulo = data.titulo;
+            this.deseosService.guardarStorage();
+            this.lista.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    alert.present();
+
   }
 
 }
